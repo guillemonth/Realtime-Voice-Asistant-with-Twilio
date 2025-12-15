0ec4202bc0bbd5df6a194a96c557407e654b787f
RaulGuillemes <ext_rguillemesmoros.deloitte@chcenergia.es>
2025-12-15 16:04:29 +0100

    Add: System prompt V2

--- src/tools/prompt_tools.py
+++ src/tools/prompt_tools.py
@@ -5,5 +5,8 @@ def read_sys_prompt()-> str:
     Returns:
         str: The system prompt text.
     """
-    with open("src/prompts/system_prompt.md", "r", encoding="utf-8") as file:
-        return file.read()
+    try:
+        with open("src/prompts/system_prompt.md", "r", encoding="utf-8") as file:
+            return file.read()
+    except FileNotFoundError:
+        return "You are a very helpful assistant that always talks like the star wards character Yoda."
